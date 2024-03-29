4f8f663e4dd12b55c07632d61479f9d90e21fab2
Isaac Buehner <165332265+isaacdbeans@users.noreply.github.com>
2024-03-28 20:11:12 -0600

    Updated GUI a bit Starting to work with actual design

--- DesignerGUI.py
+++ DesignerGUI.py
@@ -1,3 +1,5 @@
+import sys
+
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
@@ -6,8 +8,19 @@ class GUI(QMainWindow):
 
     def __init__(self):
         super(GUI, self).__init__()
-        uic.loadUi("BasicLogin.ui", self)
+        uic.loadUi("BasicLogin2.ui", self)
         self.show()
+
+        self.login_btn.clicked.connect(self.login)
+
+    def login(self):
+
+        if self.username.text() != "" and self.password.text() != "":
+            sys.exit()
+        else:
+            message = QMessageBox()
+            message.setText("Please Enter a Username and Password")
+            message.exec_()
 
 
 def main():
@@ -18,4 +31,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
