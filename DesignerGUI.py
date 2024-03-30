720a41a3d2ecb19898e36723dd2fdd5fc09dbb17
Isaac Buehner <165332265+isaacdbeans@users.noreply.github.com>
2024-03-29 22:58:07 -0600

    Create Account GUI
    Added a create account GUI and functionality between the 2 GUIs

--- DesignerGUI.py
+++ DesignerGUI.py
@@ -4,28 +4,75 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
 
-class GUI(QMainWindow):
+class LogInGUI(QMainWindow):
 
     def __init__(self):
-        super(GUI, self).__init__()
-        uic.loadUi("BasicLogin2.ui", self)
+        super(LogInGUI, self).__init__()
+        uic.loadUi("LogInGUI.ui", self)
         self.show()
 
         self.login_btn.clicked.connect(self.login)
+        self.actionClose.triggered.connect(self.close)
+        self.createAccount.clicked.connect(self.createAcc)
 
     def login(self):
 
         if self.username.text() != "" and self.password.text() != "":
             sys.exit()
+            # self.password.setEchoMode(QLineEdit.Normal)   for setting password to visible probably on a radio button
+
         else:
             message = QMessageBox()
             message.setText("Please Enter a Username and Password")
             message.exec_()
 
+    def createAcc(self):
+        self.window = CreateAccountGUI()
+        self.hide()
+        self.window.show()
+        # sys.exit()
+
+    def close(self):
+        sys.exit()
+
+
+class CreateAccountGUI(QMainWindow):
+
+    def __init__(self):
+        super(CreateAccountGUI, self).__init__()
+        uic.loadUi("CreateAccountGUI.ui", self)
+        self.show()
+
+        self.create_btn.clicked.connect(self.createAccount)
+        self.actionClose.triggered.connect(self.close)
+        self.backToLogin.clicked.connect(self.backToLog)
+
+    def createAccount(self):
+
+        if self.password.text() != self.password_2.text():
+            message = QMessageBox()
+            message.setText("Passwords do not match")
+            message.exec_()
+        else:
+            if self.username.text() != "" and self.password.text() != "":
+                sys.exit()
+            else:
+                message = QMessageBox()
+                message.setText("Please enter valid username and password")
+                message.exec_()
+
+    def backToLog(self) :
+        self.window = LogInGUI()
+        self.hide()
+        self.window.show()
+
+    def close(self):
+        sys.exit()
+
 
 def main():
     app = QApplication([])
-    window = GUI()
+    window = LogInGUI()
     app.exec_()
 
 
