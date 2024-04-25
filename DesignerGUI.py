e94c3c56d0f3d771e96dd13d75b1bbcf53d2ea3a
Isaac Buehner <165332265+isaacdbeans@users.noreply.github.com>
2024-04-24 20:32:59 -0600

    Main menu added
    Added a main menu as well as changed the color scheme of the UIs

--- DesignerGUI.py
+++ DesignerGUI.py
@@ -14,13 +14,13 @@ class LogInGUI(QMainWindow):
         self.login_btn.clicked.connect(self.login)
         self.actionClose.triggered.connect(self.close)
         self.createAccount.clicked.connect(self.createAcc)
+        self.showPassword.clicked.connect(self.showPass)
 
     def login(self):
-
         if self.username.text() != "" and self.password.text() != "":
-            sys.exit()
-            # self.password.setEchoMode(QLineEdit.Normal)   for setting password to visible probably on a radio button
-
+            self.window = MainMenu()
+            self.hide()
+            self.window.show()
         else:
             message = QMessageBox()
             message.setText("Please Enter a Username and Password")
@@ -31,6 +31,12 @@ class LogInGUI(QMainWindow):
         self.hide()
         self.window.show()
         # sys.exit()
+
+    def showPass(self):
+        if self.showPassword.isChecked():
+            self.password.setEchoMode(QLineEdit.Normal)
+        else:
+            self.password.setEchoMode(QLineEdit.Password)
 
     def close(self):
         sys.exit()
@@ -46,6 +52,8 @@ class CreateAccountGUI(QMainWindow):
         self.create_btn.clicked.connect(self.createAccount)
         self.actionClose.triggered.connect(self.close)
         self.backToLogin.clicked.connect(self.backToLog)
+        self.showPassword.clicked.connect(self.showPass)
+        self.showPassword_2.clicked.connect(self.showPass_2)
 
     def createAccount(self):
 
@@ -61,13 +69,37 @@ class CreateAccountGUI(QMainWindow):
                 message.setText("Please enter valid username and password")
                 message.exec_()
 
-    def backToLog(self) :
+    def backToLog(self):
         self.window = LogInGUI()
         self.hide()
         self.window.show()
 
+    def showPass(self):
+        if self.showPassword.isChecked():
+            self.password.setEchoMode(QLineEdit.Normal)
+        else:
+            self.password.setEchoMode(QLineEdit.Password)
+
+    def showPass_2(self):
+        if self.showPassword_2.isChecked():
+            self.password_2.setEchoMode(QLineEdit.Normal)
+        else:
+            self.password_2.setEchoMode(QLineEdit.Password)
+
     def close(self):
         sys.exit()
+
+
+class MainMenu(QMainWindow):
+    def __init__(self):
+        username: str
+
+        super(MainMenu, self).__init__()
+        uic.loadUi("MainMenu.ui", self)
+        self.show()
+        username = "Isaac"
+        self.level_progress_bar.setValue(0)
+        self.welcome_lbl.setText("Welcome " + username)
 
 
 def main():
