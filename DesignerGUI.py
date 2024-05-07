2a187937da32ddb43d15dbd63b4a8d7ac69ba6d8
Isaac Buehner <165332265+isaacdbeans@users.noreply.github.com>
2024-05-07 15:27:39 -0600

    Firebase functionality
    Created a firebase project under my email isaacdbeans@gmail.com
    Added firebase functionality in the login and create account GUIs
    Functionality in main menu is next

--- DesignerGUI.py
+++ DesignerGUI.py
@@ -1,12 +1,24 @@
 import sys
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
 
+# Global variables for the firebase data
+cred = credentials.Certificate('sign-language-user-data-da9ba1b8df23.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+global current_account
+
+
 class LogInGUI(QMainWindow):
 
     def __init__(self):
+
+        # opening gui and connecting buttons
         super(LogInGUI, self).__init__()
         uic.loadUi("LogInGUI.ui", self)
         self.show()
@@ -17,22 +29,46 @@ class LogInGUI(QMainWindow):
         self.showPassword.clicked.connect(self.showPass)
 
     def login(self):
-        if self.username.text() != "" and self.password.text() != "":
-            self.window = MainMenu()
-            self.hide()
-            self.window.show()
-        else:
+
+        # username and password must not be empty
+        if self.username.text() == "" or self.password.text() == "":
             message = QMessageBox()
             message.setText("Please Enter a Username and Password")
             message.exec_()
+            return
+
+        doc_ref = db.collection("users").document(self.username.text())
+        doc = doc_ref.get()
+
+        # username has to exist as a document in the firebase data
+        if not doc.exists:
+            message = QMessageBox()
+            message.setText("Username or password incorrect")
+            message.exec_()
+
+        # check for input password matching firebase password
+        elif self.password.text() != str(doc.to_dict()["password"]):
+            message = QMessageBox()
+            message.setText("Username or password incorrect")
+            message.exec_()
+
+        # setcurrent account and open menu gui
+        else:
+            global current_account
+            current_account = self.username.text()
+            self.window = MainMenu()
+            self.hide()
+            self.window.show()
 
     def createAcc(self):
+        # opens create account gui
         self.window = CreateAccountGUI()
         self.hide()
         self.window.show()
         # sys.exit()
 
     def showPass(self):
+        # toggles password visibility
         if self.showPassword.isChecked():
             self.password.setEchoMode(QLineEdit.Normal)
         else:
@@ -45,6 +81,7 @@ class LogInGUI(QMainWindow):
 class CreateAccountGUI(QMainWindow):
 
     def __init__(self):
+        # opening gui and connecting buttons
         super(CreateAccountGUI, self).__init__()
         uic.loadUi("CreateAccountGUI.ui", self)
         self.show()
@@ -53,34 +90,58 @@ class CreateAccountGUI(QMainWindow):
         self.actionClose.triggered.connect(self.close)
         self.backToLogin.clicked.connect(self.backToLog)
         self.showPassword.clicked.connect(self.showPass)
-        self.showPassword_2.clicked.connect(self.showPass_2)
+        self.showPassword_2.clicked.connect(self.showPassConfirm)
 
     def createAccount(self):
-
+        # passwords must match
         if self.password.text() != self.password_2.text():
             message = QMessageBox()
             message.setText("Passwords do not match")
             message.exec_()
+
+        # username and password must both be longer than 7 chars
+        elif len(self.username.text()) < 7 or len(self.password.text()) < 7:
+            message = QMessageBox()
+            message.setText("Username and password must be more than 7 characters")
+            message.exec_()
         else:
-            if self.username.text() != "" and self.password.text() != "":
-                sys.exit()
-            else:
+            doc_ref = db.collection("users").document(self.username.text())
+            doc = doc_ref.get()
+
+            # if the input username matches a document name in the database
+            if doc.exists:
                 message = QMessageBox()
-                message.setText("Please enter valid username and password")
+                message.setText("Username already exists")
                 message.exec_()
 
+            # add the given data to a new document in the database
+            else:
+                data = {"username": self.username.text(), "password": self.password.text()}
+                doc_ref.set(data)
+                message = QMessageBox()
+                message.setText("Account Created")
+                message.exec_()
+                # opens the login gui
+                self.window = LogInGUI()
+                self.hide()
+                self.window.show()
+
+
     def backToLog(self):
+        # opens the login gui
         self.window = LogInGUI()
         self.hide()
         self.window.show()
 
     def showPass(self):
+        # toggles visibility for password 1
         if self.showPassword.isChecked():
             self.password.setEchoMode(QLineEdit.Normal)
         else:
             self.password.setEchoMode(QLineEdit.Password)
 
-    def showPass_2(self):
+    def showPassConfirm(self):
+        # toggles visibility for password 2
         if self.showPassword_2.isChecked():
             self.password_2.setEchoMode(QLineEdit.Normal)
         else:
@@ -97,15 +158,15 @@ class MainMenu(QMainWindow):
         super(MainMenu, self).__init__()
         uic.loadUi("MainMenu.ui", self)
         self.show()
-        username = "Isaac"
+        doc_ref = db.collection("users").document(current_account)
         self.level_progress_bar.setValue(0)
-        self.welcome_lbl.setText("Welcome " + username)
+        self.welcome_lbl.setText("Welcome " + current_account)
 
 
 def main():
-    app = QApplication([])
+    main_app = QApplication([])
     window = LogInGUI()
-    app.exec_()
+    main_app.exec_()
 
 
 if __name__ == "__main__":
